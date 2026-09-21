9ae7047dd389a5c29889593964b23b8d0069b256
leraos <https://github.com/leraos>
2026-09-21 19:12:21 +0100

    continuing work on sql inventory notebook

--- sql/01_inventory.sql
+++ sql/01_inventory.sql
@@ -1,4 +1,16 @@
-SHOW TABLES;
-select * 
+-- 01_inventory.sql
+-- Inventory SQL training on FORCE 2020 dataset
+
+-- ### retrieval
+
+-- select * 
+select file, curves
 from well_inventory
-limit 5;
+limit 7;
+
+
+-- ### filtering rows 
+
+select *
+from curve_inventory
+where missing_pct >30;
